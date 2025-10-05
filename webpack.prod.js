c27e72b0175c15b60334e6dab8bfecf0deb98736
Ryan Goh <ry4n.gch@gmail.com>
2025-10-05 15:00:29 +0800

    fixed assets and images folders for dummy datas

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -19,4 +20,22 @@ module.exports = merge(common, {
     minimize: true,
     splitChunks: { chunks: 'all' },
   },
+  plugins: [
+    // copy every files and folders during production build except for index.html
+    // we need assets and images folder here to be copied for some features to work
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: '.',
+          globOptions: {
+            ignore: ['**/index.html'], // don't copy index.html
+            dot: true,                 // Include hidden files (like .htaccess) if any
+          },
+          noErrorOnMissing: true, // Ignore missing source dirs/files
+          force: true,            // Ensures overwrites are allowed
+        },
+      ],
+    }),
+  ] 
 });
